6d0756b6446743d411af665bd3b945c7c58ac8d5
Hironori43 <hironori.ito@goodsun-hd.com>
2024-06-17 13:19:14 +0900

    settings

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+ALLOWED_HOSTS = ['simpletodoapp.azurewebsites.net', 'anotherhost.com', 'localhost']
